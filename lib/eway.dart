b6140b68e96bfbf8cadfe581b668a9d02441d601
Hilmi Berkay Ozdemir <59442397+hberkayozdemir@users.noreply.github.com>
2024-10-21 02:00:52 +0300

    first commit

---
+++ lib/eway.dart
@@ -0,0 +1,10 @@
+/// eway, A Very Good Project created by Very Good CLI.
+///
+/// ```sh
+/// # activate eway
+/// dart pub global activate eway
+///
+/// # see usage
+/// eway --help
+/// ```
+library;
